e1a4a0fc0da13d32c3f81ba608da22658b706f8a
m-amroune <amroune.moustapha@gmail.com>
2022-08-27 21:34:12 +0200

    deploy app

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,30 +10,38 @@ const Carousel = ({ accomodation }) => {
   if (position === 0 && pictures.length > 1) {
     return (
       <div className="background-slider">
-        <img className="cover-slider" src={pictures[0]} alt="" />
-
+        <figure>
+          <img className="cover-slider" src={pictures[0]} alt="" />
+        </figure>
         <button
           className="left-slider"
           onClick={() => setPosition(pictures.length - 1)}
         >
-          <img className="img-left-slider" src={Left} alt="" />
+          <figure>
+            <img className="img-left-slider" src={Left} alt="" />
+          </figure>
         </button>
         <button
           className="right-slider"
           onClick={() => setPosition(position + 1)}
         >
-          <img className="img-right-slider" src={Right} alt="" />
+          <figure>
+            <img className="img-right-slider" src={Right} alt="" />
+          </figure>
         </button>
       </div>
     );
   } else if (position > 0) {
     return (
       <div className="background-slider">
-        <img
-          className="cover-slider"
-          src={pictures[position]}
-          alt="hébergement"
-        />
+        <figure>
+          <img
+            className="cover-slider"
+            src={pictures[position]}
+            alt="hébergement"
+          />
+        </figure>
+
         <button
           className="left-slider"
           onClick={() =>
@@ -42,7 +50,9 @@ const Carousel = ({ accomodation }) => {
               : setPosition(pictures.length - 1)
           }
         >
-          <img className="img-left-slider" src={Left} alt="" />
+          <figure>
+            <img className="img-left-slider" src={Left} alt="" />
+          </figure>
         </button>
         <button
           className="right-slider"
@@ -54,15 +64,17 @@ const Carousel = ({ accomodation }) => {
         >
           <img className="img-right-slider" src={Right} alt="" />
         </button>
-        <div className="slides-counter">
+        <figcaption className="slides-counter">
           {(position + 1).toString()}/{pictures.length.toString()}
-        </div>
+        </figcaption>
       </div>
     );
   } else {
     return (
       <div className="background-slider">
-        <img className="cover-slider" src={accomodation.pictures[0]} alt="" />
+        <figure>
+          <img className="cover-slider" src={accomodation.pictures[0]} alt="" />
+        </figure>
       </div>
     );
   }
