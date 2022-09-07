bee55c23f5842f6cede28620d679f7f01620c6f6
m-amroune <amroune.moustapha@gmail.com>
2022-09-07 20:51:57 +0200

    update css cards

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,18 +1,17 @@
 import React from "react";
 import data from "./../data/data.json";
 import Header from "../components/Header";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import Carousel from "../components/Carousel";
 import Info from "../components/Info";
 import Footer from "../components/Footer";
-import Error from "./Error";
 
 const Details = () => {
   const { id } = useParams();
   const getId = data.filter((accomodation) => accomodation.id === id);
 
   if (getId.length === 0) {
-    return <Error />;
+    return <Navigate to="*" />;
   }
 
   return getId.map((accomodation, id) => (
